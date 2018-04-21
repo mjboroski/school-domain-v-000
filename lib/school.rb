0aee981370aac6952f9bfa4540468c77e145c579
Michael Boroski <mjboroski@gmail.com>
2018-04-21 23:05:17 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,14 @@
 class School
 
-  attr_accessor :roster,:name,:grade
+  attr_accessor :roster,:student
 
   def initialize(name)
     @name=name
   end
 
   def add_student(name, grade)
-    :roster[@name]=name
-    :roster[@grade]=grade
+    :student[@name]=name
+    :student[@grade]=grade
   end
 
 
